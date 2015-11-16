e724b549e19de90c81b031a81091c42e254c2fdb
nishantgupta91 <nishant8568@gmail.com>
2015-11-16 08:06:21 +0100

    Prototype for offline assistance

---
+++ HelperPrototypeOffline/app/js/directives.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Nishant on 8/28/2015.
+ */
+angular.module('mainApp')
+    .directive('mainHeaderToolbar', function() {
+        return {
+            restrict: "E",
+            templateUrl: "app/partials/mainHeader.html"
+        }
+    })
+    .directive('myToolbox', function() {
+        return {
+            restrict: "E",
+            $scope: {
+                name: "="
+            },
+            templateUrl: "app/partials/toolbox.html",
+            controller: "ToolboxController"
+        }
+    })
+    .directive('clearButton', function() {
+        return {
+            restrict: "E",
+            templateUrl: "app/partials/clearButton.html",
+            controller: "ClearOptionsController"
+        }
+    })
+    .directive('selectVideoButton', function() {
+        return {
+            restrict: "E",
+            templateUrl: "app/partials/selectVideoButton.html",
+            controller: "SelectVideoOptionsController"
+        }
+    })
+    .directive('videoPlayer', function() {
+        return {
+            restrict: "E",
+            templateUrl: "app/partials/videoPlayer.html"
+        }
+    });
