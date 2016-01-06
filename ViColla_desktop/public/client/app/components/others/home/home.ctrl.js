e1068db06d4d8ef306782910fa2b91eccd259c03
nishantgupta91 <nishant8568@gmail.com>
2016-01-06 17:34:46 +0100

    added remaining login files to git

---
+++ ViColla_desktop/public/client/app/components/others/home/home.ctrl.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Antony on 11/21/2015.
+ */
+homeModule.controller('HomeController', ['$scope', 'authService', '$window', '$location', 'socket', 'utilityService',
+    function ($scope, authService, $window, $location, socket, utilityService) {
+        'use strict';
+
+        var vm = this;
+        vm.user = {
+            username: "",
+            password: ""
+        };
+
+        vm.submitCredentials = function () {
+            authService.login(vm.user)
+                .then(function (data) {
+                    if (data.success) {
+                        $scope.appCtrl.user = data["user"];
+                        localStorage.setItem('username', $scope.appCtrl.user.username);
+                        console.log("emit user login");
+                        $scope.appCtrl.loadContacts();
+                        utilityService.setExpertFlag($scope.appCtrl.user.isExpert);
+                        socket.emit('userLogin', $scope.appCtrl.user.username);
+                        if (utilityService.getExpertFlag()) {
+                            $location.path('/callHistory');
+                        } else {
+                            $location.path('/experts');
+                        }
+                    } else {
+                        $window.alert(data["message"]);
+                    }
+                })
+        };
+
+        vm.test_session = function () {
+            authService.test_session()
+
+        };
+
+        vm.logout = function () {
+            authService.logout()
+
+        };
+    }]);
